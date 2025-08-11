7430a819b34b997ac7aaea70fe337914b2347c60
dgdsingen <dgdsingen@gmail.com>
2025-08-11 16:31:28 +0900

    optimize: remove bufio, use slice as buffer & write on io.Writer directly.

--- cmd/r2n/main.go
+++ cmd/r2n/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -98,23 +97,14 @@ func replaceRN(bs []byte) []byte {
 	return bs[:p]
 }
 
-func writeAndFlushAll(dst *bufio.Writer, bs ...[]byte) {
-	for _, b := range bs {
-		dst.Write(b)
-	}
-	dst.Flush()
-}
-
 func copyAndReplace(dst io.Writer, src io.Reader, prefix *string) {
 	const maxLineLength = 64 * 1024 // 64KB
 
 	buf := make([]byte, 4096)
 	// len > 0 이면 slice가 zero value로 채워져서 이상하게 출력될 수 있으므로 0으로 설정
 	out := make([]byte, 0, 4096)
-	// system call을 줄이기 위해 라인 단위로 버퍼링해서 출력
-	dstBuf := bufio.NewWriter(dst)
 	bprefix := []byte(*prefix)
-	bn := []byte{'\n'}
+	bn := byte('\n')
 
 	for {
 		n, err := src.Read(buf)
@@ -127,7 +117,8 @@ func copyAndReplace(dst io.Writer, src io.Reader, prefix *string) {
 			p := 0
 			for i, b := range out {
 				if b == '\n' {
-					writeAndFlushAll(dstBuf, bprefix, out[p:i+1])
+					line := append(bprefix, out[p:i+1]...)
+					dst.Write(line)
 					p = i + 1
 				}
 			}
@@ -141,7 +132,9 @@ func copyAndReplace(dst io.Writer, src io.Reader, prefix *string) {
 
 			// chunk가 '\n' 없이 계속 들어올때 out 무한 증가를 막기 위해 강제 라인처리 + flush
 			if len(out) > maxLineLength {
-				writeAndFlushAll(dstBuf, bprefix, out, bn)
+				line := append(bprefix, out...)
+				line = append(line, bn)
+				dst.Write(line)
 				out = out[:0]
 			}
 		}
@@ -149,7 +142,9 @@ func copyAndReplace(dst io.Writer, src io.Reader, prefix *string) {
 		if err != nil {
 			// '\n' 없이 끝난 경우 강제로 라인 처리해서 내보냄
 			if len(out) > 0 {
-				writeAndFlushAll(dstBuf, bprefix, out, bn)
+				line := append(bprefix, out...)
+				line = append(line, bn)
+				dst.Write(line)
 			}
 			break
 		}
