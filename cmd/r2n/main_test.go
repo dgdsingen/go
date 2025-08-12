8badd92fd245882c35c0aa9f6635a6461fa1b4c6
dgdsingen <dgdsingen@gmail.com>
2025-08-12 17:08:17 +0900

    update test comments

--- cmd/r2n/main_test.go
+++ cmd/r2n/main_test.go
@@ -11,24 +11,24 @@ import (
 //   bytes.Cuts가 가장 빠르고 가벼우며, bytes.Replace 사용시에는 메모리가 급증한다.
 //   bytes.IndexAny + 커스텀 로직 조합보다 그냥 최적화된 표준 라이브러리 쓰는게 낫다.
 //   slice 재할당은 성능과 메모리에 치명적이다.
-// BenchmarkLongLinesParseCuts-4                     1   1010934943 ns/op   1000170864 B/op   10 allocs/op
+// BenchmarkLongLinesParseCuts-4                     1   1010934943 ns/op   1000170864 B/op       10 allocs/op
 // BenchmarkLongLinesParseReplaceCut-4               1   1576265115 ns/op   3000379056 B/op   488343 allocs/op
 // BenchmarkLongLinesParseReplaceSplit-4             1   1664624135 ns/op   3008639040 B/op   732509 allocs/op
-// BenchmarkLongLinesParseScanner-4                  1   2348610999 ns/op   1000156496 B/op   9 allocs/op
-// BenchmarkLongLinesParseIndexAny-4                 1   2742300694 ns/op   1000170864 B/op   10 allocs/op
+// BenchmarkLongLinesParseScanner-4                  1   2348610999 ns/op   1000156496 B/op        9 allocs/op
+// BenchmarkLongLinesParseIndexAny-4                 1   2742300694 ns/op   1000170864 B/op       10 allocs/op
 // BenchmarkLongLinesParseReplaceSlice-4             1   3864148592 ns/op   4365047184 B/op   344174 allocs/op
-// BenchmarkMixedLinesParseCuts-4           1000000000      0.09428 ns/op            0 B/op   0 allocs/op
-// BenchmarkMixedLinesParseIndexAny-4       1000000000       0.1936 ns/op            0 B/op   0 allocs/op
-// BenchmarkMixedLinesParseReplaceSlice-4   1000000000       0.2141 ns/op            0 B/op   0 allocs/op
-// BenchmarkMixedLinesParseReplaceCut-4     1000000000       0.2193 ns/op            0 B/op   0 allocs/op
-// BenchmarkMixedLinesParseReplaceSplit-4   1000000000       0.2406 ns/op            0 B/op   0 allocs/op
-// BenchmarkMixedLinesParseScanner-4                 1   1145075522 ns/op      1405008 B/op   5 allocs/op
-// BenchmarkShortLinesParseReplaceSplit-4   1000000000       0.1077 ns/op            0 B/op   0 allocs/op
-// BenchmarkShortLinesParseReplaceCut-4     1000000000       0.1167 ns/op            0 B/op   0 allocs/op
-// BenchmarkShortLinesParseCuts-4           1000000000       0.1228 ns/op            0 B/op   0 allocs/op
-// BenchmarkShortLinesParseIndexAny-4       1000000000       0.1364 ns/op            0 B/op   0 allocs/op
-// BenchmarkShortLinesParseReplaceSlice-4   1000000000       0.1499 ns/op            0 B/op   0 allocs/op
-// BenchmarkShortLinesParseScanner-4        1000000000       0.3072 ns/op            0 B/op   0 allocs/op
+// BenchmarkMixedLinesParseCuts-4           1000000000      0.09428 ns/op            0 B/op        0 allocs/op
+// BenchmarkMixedLinesParseIndexAny-4       1000000000       0.1936 ns/op            0 B/op        0 allocs/op
+// BenchmarkMixedLinesParseReplaceSlice-4   1000000000       0.2141 ns/op            0 B/op        0 allocs/op
+// BenchmarkMixedLinesParseReplaceCut-4     1000000000       0.2193 ns/op            0 B/op        0 allocs/op
+// BenchmarkMixedLinesParseReplaceSplit-4   1000000000       0.2406 ns/op            0 B/op        0 allocs/op
+// BenchmarkMixedLinesParseScanner-4                 1   1145075522 ns/op      1405008 B/op        5 allocs/op
+// BenchmarkShortLinesParseReplaceSplit-4   1000000000       0.1077 ns/op            0 B/op        0 allocs/op
+// BenchmarkShortLinesParseReplaceCut-4     1000000000       0.1167 ns/op            0 B/op        0 allocs/op
+// BenchmarkShortLinesParseCuts-4           1000000000       0.1228 ns/op            0 B/op        0 allocs/op
+// BenchmarkShortLinesParseIndexAny-4       1000000000       0.1364 ns/op            0 B/op        0 allocs/op
+// BenchmarkShortLinesParseReplaceSlice-4   1000000000       0.1499 ns/op            0 B/op        0 allocs/op
+// BenchmarkShortLinesParseScanner-4        1000000000       0.3072 ns/op            0 B/op        0 allocs/op
 
 var (
 	count  = 100000
