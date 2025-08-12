3303076e695320a01fc79b5e6f62bbe8e75b1c4a
dgdsingen <dgdsingen@gmail.com>
2025-08-12 16:36:33 +0900

    version const > var

--- cmd/r2n/config.go
+++ cmd/r2n/config.go
@@ -2,8 +2,8 @@ package main
 
 const maxLineLength = 64 * 1024 // 64KB
 const appName = "r2n"
-const version = "undefined"
 
+var version = "undefined"
 var bn = []byte{'\n'}
 var br = []byte{'\r'}
 var bnn = []byte{'\n', '\n'}
