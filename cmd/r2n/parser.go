a4fe81d1edef1bdd25cabea2ac375d15db7cbcbf
dgdsingen <dgdsingen@gmail.com>
2025-08-12 12:29:43 +0900

    apply cut on parser

--- cmd/r2n/parser.go
+++ cmd/r2n/parser.go
@@ -22,34 +22,44 @@ func copyAndReplace(dst io.Writer, src io.Reader, prefix string) {
 	line := new(bytes.Buffer)
 
 	bprefix := []byte(prefix)
-	br := []byte{'\r'}
 	bn := []byte{'\n'}
-	bnn := []byte{'\n', '\n'}
+	br := []byte{'\r'}
 
 	for {
 		n, err := src.Read(buf)
 		if n > 0 {
-			chunk := buf[:n]
-			chunk = bytes.ReplaceAll(chunk, br, bn)
-			chunk = bytes.ReplaceAll(chunk, bnn, bn)
-			stream.Write(chunk)
+			stream.Write(buf[:n])
 
-			// 예를 들어 "12\n34\n5" 중 "12", "34"는 각각의 라인으로 잘라서 전송하고
-			split := bytes.Split(stream.Bytes(), bn)
-			for _, s := range split[:len(split)-1] {
-				dst.Write(concatBytes(line, bprefix, s, bn))
-			}
-
-			// 마지막 5는 아직 라인이 미완성이므로 버퍼에 남겨둠
-			last := split[len(split)-1]
-			stream.Reset()
-			stream.Write(last)
-
-			// chunk가 '\n' 없이 계속 들어올때 out 무한 증가를 막기 위해 강제 라인처리 + flush
+			// chunk가 '\r' or '\n' 없이 계속 들어올때 out 무한 증가하지 않게 강제로 라인 Write
 			if stream.Len() > maxLineLength {
 				dst.Write(concatBytes(line, bprefix, stream.Bytes(), bn))
 				stream.Reset()
+				continue
 			}
+
+			// 예를 들어 "12\n34\n5" 중 "12", "34"는 각각의 라인으로 잘라서 전송하고
+			sBytes := stream.Bytes()
+			for {
+				// '\r' 로 먼저 Cut 해보고
+				before, after, found := bytes.Cut(sBytes, br)
+				if !found {
+					// 못찾았으면 '\n' 로 다시 Cut
+					before, after, found = bytes.Cut(sBytes, bn)
+				}
+
+				if found {
+					if len(before) > 0 {
+						dst.Write(concatBytes(line, bprefix, before, bn))
+					}
+					sBytes = after
+				} else {
+					break
+				}
+			}
+
+			// 마지막 5는 아직 라인이 미완성이므로 버퍼에 남겨둠
+			stream.Reset()
+			stream.Write(sBytes)
 		}
 
 		if err != nil {
