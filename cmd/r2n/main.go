c50f7fe626ff01d5c739a1ef19b5d9c23d7df69f
dgdsingen <dgdsingen@gmail.com>
2025-08-12 09:25:21 +0900

    split parser from main

--- cmd/r2n/main.go
+++ cmd/r2n/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -80,61 +79,5 @@ func main() {
 
 	if err := subCmd.Wait(); err != nil {
 		os.Exit(subCmd.ProcessState.ExitCode())
-	}
-}
-
-func concatBytes(bs ...[]byte) []byte {
-	// system call을 줄이기 위해 라인 단위로 버퍼링해서 출력. 이게 bufio.Writer 보다 빠름
-	line := new(bytes.Buffer)
-	for _, b := range bs {
-		line.Write(b)
-	}
-	return line.Bytes()
-}
-
-func copyAndReplace(dst io.Writer, src io.Reader, prefix string) {
-	const maxLineLength = 64 * 1024 // 64KB
-
-	buf := make([]byte, 4096)
-	stream := new(bytes.Buffer)
-
-	bprefix := []byte(prefix)
-	br := []byte{'\r'}
-	bn := []byte{'\n'}
-	bnn := []byte{'\n', '\n'}
-
-	for {
-		n, err := src.Read(buf)
-		if n > 0 {
-			chunk := buf[:n]
-			chunk = bytes.ReplaceAll(chunk, br, bn)
-			chunk = bytes.ReplaceAll(chunk, bnn, bn)
-			stream.Write(chunk)
-
-			// 예를 들어 "12\n34\n5" 중 "12", "34"는 각각의 라인으로 잘라서 전송하고
-			split := bytes.Split(stream.Bytes(), bn)
-			for _, s := range split[:len(split)-1] {
-				dst.Write(concatBytes(bprefix, s, bn))
-			}
-
-			// 마지막 5는 아직 라인이 미완성이므로 버퍼에 남겨둠
-			last := split[len(split)-1]
-			stream.Reset()
-			stream.Write(last)
-
-			// chunk가 '\n' 없이 계속 들어올때 out 무한 증가를 막기 위해 강제 라인처리 + flush
-			if stream.Len() > maxLineLength {
-				dst.Write(concatBytes(bprefix, stream.Bytes(), bn))
-				stream.Reset()
-			}
-		}
-
-		if err != nil {
-			// '\n' 없이 끝난 경우 강제로 라인 처리해서 내보냄
-			if stream.Len() > 0 {
-				dst.Write(concatBytes(bprefix, stream.Bytes(), bn))
-			}
-			break
-		}
 	}
 }
