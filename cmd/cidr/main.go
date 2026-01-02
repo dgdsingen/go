580df36ae05a6aeb6c7276048a5bb307857f936d
dgdsingen <dgdsingen@gmail.com>
2026-01-02 18:05:43 +0900

    cidr: add -l (List ips from cidr)

--- cmd/cidr/main.go
+++ cmd/cidr/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"net"
+	"net/netip"
 	"os"
 	"strings"
 )
+
+type IP struct {
+	netip.Addr
+	inPrefix bool
+}
 
 const appName = "cidr"
 
@@ -16,13 +22,30 @@ func fmtVersion() string {
 	return fmt.Sprintf("%s %s", appName, version)
 }
 
-type IP struct {
-	net.IP
-	inCidr bool
+func SplitPrefixAddr(args []string) (ips []IP, prefixes []netip.Prefix, err error) {
+	ips = make([]IP, 0, 1)
+	prefixes = make([]netip.Prefix, 0, 1)
+	for i := range args {
+		if strings.Contains(args[i], "/") {
+			prefix, err := netip.ParsePrefix(args[i])
+			if err != nil {
+				return ips, prefixes, errors.New("Invalid cidr: " + args[i])
+			}
+			prefixes = append(prefixes, prefix)
+		} else {
+			addr, err := netip.ParseAddr(args[i])
+			if err != nil {
+				return ips, prefixes, errors.New("Invalid ip: " + args[i])
+			}
+			ips = append(ips, IP{Addr: addr})
+		}
+	}
+	return ips, prefixes, nil
 }
 
 func main() {
 	v := flag.Bool("v", false, "Invert match")
+	l := flag.Bool("l", false, "List ips from cidr")
 	versionFlag := flag.Bool("version", false, "Version")
 	flag.Parse()
 
@@ -31,53 +54,63 @@ func main() {
 		return
 	}
 
+	if *l {
+		if len(os.Args) < 3 {
+			fmt.Fprintf(os.Stderr, "Usage: %s -l <cidr...>\n", os.Args[0])
+			os.Exit(1)
+		}
+
+		_, prefixes, err := SplitPrefixAddr(flag.Args())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+		if len(prefixes) == 0 {
+			fmt.Fprintf(os.Stderr, "No cidr.\n")
+			os.Exit(1)
+		}
+
+		for p := range prefixes {
+			addr := prefixes[p].Addr()
+			for addr.IsValid() && prefixes[p].Contains(addr) {
+				fmt.Println(addr)
+				addr = addr.Next()
+			}
+		}
+		return
+	}
+
 	if len(os.Args) < 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <ip...> <cidr...>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	args := flag.Args()
-	ipSlice := make([]IP, 0, 1)
-	cidrSlice := make([]*net.IPNet, 0, 1)
-	for i := range args {
-		if strings.Contains(args[i], "/") {
-			_, cidr, err := net.ParseCIDR(args[i])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Invalid cidr: %s\n", args[i])
-				os.Exit(1)
-			}
-			cidrSlice = append(cidrSlice, cidr)
-		} else {
-			ip := net.ParseIP(args[i])
-			if ip == nil {
-				fmt.Fprintf(os.Stderr, "Invalid ip: %s\n", args[i])
-				os.Exit(1)
-			}
-			ipSlice = append(ipSlice, IP{IP: ip})
-		}
+	ips, prefixes, err := SplitPrefixAddr(flag.Args())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
-
-	if len(ipSlice) == 0 {
+	if len(ips) == 0 {
 		fmt.Fprintf(os.Stderr, "No ip.\n")
-		return
+		os.Exit(1)
 	}
-	if len(cidrSlice) == 0 {
+	if len(prefixes) == 0 {
 		fmt.Fprintf(os.Stderr, "No cidr.\n")
-		return
+		os.Exit(1)
 	}
 
-	for i := range ipSlice {
-		for c := range cidrSlice {
-			if cidrSlice[c].Contains(ipSlice[i].IP) {
-				ipSlice[i].inCidr = true
+	for i := range ips {
+		for p := range prefixes {
+			if prefixes[p].Contains(ips[i].Addr) {
+				ips[i].inPrefix = true
 			}
 		}
 	}
 
 	result := make([]string, 0, 1)
-	for i := range ipSlice {
-		if ipSlice[i].inCidr != *v {
-			result = append(result, ipSlice[i].String())
+	for i := range ips {
+		if ips[i].inPrefix != *v {
+			result = append(result, ips[i].String())
 		}
 	}
 	if len(result) == 0 {
